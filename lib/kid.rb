3a728b1f7b80310df5ec4e236382708a653aca5f
Ryan M <rmccauleymusic@gmail.com>
2019-11-24 18:58:11 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
+  extend MetaDancing
   include Dance
 
   attr_accessor :name
@@ -10,5 +11,5 @@ class Kid
     @name = name
   end
 
-  
+
 end
